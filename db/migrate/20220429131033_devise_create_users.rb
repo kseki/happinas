e6d1a734fa48e93519983b9d6939cb0ce820c2fd
kseki <kota.yt@gmail.com>
2022-05-02 09:14:21 +0900

    Rubocop通るように修正

--- db/migrate/20220429131033_devise_create_users.rb
+++ db/migrate/20220429131033_devise_create_users.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
+  # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
   def change
     create_table :users do |t|
       ## Database authenticatable
@@ -40,4 +41,5 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
     # add_index :users, :confirmation_token,   unique: true
     add_index :users, :unlock_token,         unique: true
   end
+  # rubocop:enable Metrics/AbcSize,Metrics/MethodLength
 end
